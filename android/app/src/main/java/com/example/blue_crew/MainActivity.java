af72034e050ae5fb9b1475750730d70ef306aeb9
Shubh Harde <shubhharde158@gmail.com>
2023-09-23 09:51:46 +0530

    Initial Commit

---
+++ android/app/src/main/java/com/example/blue_crew/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.blue_crew;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+
+const Color kYellow = Color(0xffffcb74);
+const Color kBrown = Color(0xff2f2f2f);
+const Color kWhite = Color(0xfff6f6f6);
+const Color kBlack = Color(0xff111111);
+
